7ab11b05b208d5d662c4b13ea305cd62c7051d77
sue407 <sue407407@gmail.com>
2022-11-08 16:44:21 +0900

    09 (5)
    11/8 (5)

--- main.c
+++ main.c
@@ -4,14 +4,25 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i = 0;
-	char str[4];
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = '\0';
+	int a, b, c;
 	
-	printf("%s\n", str);
+	printf("input a word: ");
+	 scanf("%c", &a);
+	
+	printf("input a word: ");
+	 scanf("\n %c", &b);
+
+	printf("input a word: ");
+	 scanf("\n %c", &c);
+
+	
+	FILE*fp;
+	fp = fopen("sample.txt", "w");
+    fprintf(fp, "%c \n", a);
+    fprintf(fp, "%c \n", b);
+    fprintf(fp, "%c", c);
+	
+	fclose(fp);
 	
 	return 0;
 }
