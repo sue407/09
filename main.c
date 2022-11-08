1b08c24d58765a2310f26f084354e6dfe5fac11d
sue407 <sue407407@gmail.com>
2022-11-08 16:38:51 +0900

    09 (2)
    11/8 (2)

--- main.c
+++ main.c
@@ -4,8 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char str[30] = "happy C programming";
+	int i = 0;
+	char str[4];
+	str[0] = 'a';
+	str[1] = 'b';
+	str[2] = 'c';
+	str[3] = '\0';
 	
-	printf("문자열\"%s\"의 길이 : %i", str, strlen(str));
+	printf("%s\n", str);
+	
 	return 0;
 }
