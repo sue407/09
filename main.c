c85fb35e1156641b1e3d6d3033570e3f6966dbba
sue407 <sue407407@gmail.com>
2022-11-08 16:05:52 +0900

    09 (3)
    11/8 (3)

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char code1 = 'A';
-    int code2 = 65;
-    printf("code1 = %c, (%d)\n", code1, code1);
-    printf("code2 = %c, (%d)\n", code2, code2);
+	char src[] = "The worst things to eat before you sleep";
+	char dst[100];
+	strcpy(dst, src);
 	
+	printf("copied string : %s", dst);
 	return 0;
 }
